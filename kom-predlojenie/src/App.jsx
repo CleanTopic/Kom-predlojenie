08504a31107523c8caa5cdfa58c4834bc74cab0b
Danila <cleantopic@yandex.ru>
2026-03-11 11:34:21 +0300

    added generation pdf added id for page added new css style for A4 papper

--- kom-predlojenie/src/App.jsx
+++ kom-predlojenie/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Page1 from './component/Page1'
+import html2pdf from 'html2pdf.js'
 
 const items = [{
   id: 1,
@@ -42,13 +43,35 @@ const total = {
   quantity: 1,
   price: 20000,
   discountAmount: 2000,
-  finalPrice: 18000
+  finalPrice: 18000,
+  lastDayOffer: '01.01.2025'
 }
 
 
 function App() {
+
+  const generatePDF = () => {
+    const option = {
+      margin: 0,
+      filename: 'offer.pdf',
+      image: { type: 'jpeg', quality: 0.98 },
+      html2canvas: { scale: 3 },
+      jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+    }
+
+    const element = document.getElementById('pdf');
+    
+    html2pdf().set(option).from(element).save("offer.pdf");
+  }
+
   return (
-    <Page1 items={items} total={total} />
+
+    <div>
+    <div id="pdf"> 
+      <Page1 items={items} total={total} />
+    </div>
+    <button onClick={generatePDF}>Сгенерировать PDF</button>
+    </div>
   )
 }
 
